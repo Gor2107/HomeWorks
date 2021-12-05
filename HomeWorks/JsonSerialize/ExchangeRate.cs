60e906fad3937b58c62d3e2cd9308f7ec6dd4ea9
Gor <GOR19842107@GMAIL.COM>
2021-12-06 00:29:59 +0400

    json

---
+++ HomeWorks/JsonSerialize/ExchangeRate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+
+namespace ExchangeRates
+{
+
+
+    [Serializable]
+    public class ExchangeRate
+    {
+        public bool success { get; set; }
+        public long timestamp { get; set; }
+        public string Base { get; set; }
+        public DateTime date { get; set; }
+        public Dictionary<string,double> rates { get; set; }
+
+
+    }
+
+}
---
+++ HomeWorks/JsonSerialize/Program.cs
@@ -0,0 +1,31 @@
+﻿using ExchangeRates;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+//using System.Text.Json.Serialization;
+
+namespace JsonSerialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string fileName= "C:\\Users\\User\\Desktop\\Rates.json";
+            string jsonString = File.ReadAllText(fileName);
+            ExchangeRate tmp = JsonSerializer.Deserialize<ExchangeRate>(jsonString);
+            string regexPattern = @"base";
+            Match next = Regex.Match(jsonString,regexPattern);
+          tmp.Base = jsonString.Substring(next.Index+7,3);
+
+            foreach (var item in tmp.rates)
+            {
+                Console.WriteLine(item.Key + "   " + Math.Round(item.Value, 3));
+            }
+
+            Console.WriteLine(tmp.Base);
+            Console.WriteLine(tmp.rates[tmp.Base]);
+        }
+    }
+}
